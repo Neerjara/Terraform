8dfc52bc3ea327e7ea4238a582d6eb4834d93016
Neerjara <55031473+Neerjara@users.noreply.github.com>
2026-07-15 21:51:52 +0530

    Add Azure Virtual Network and Subnet resources

---
+++ 3-tier architecture/vnet.tf
@@ -0,0 +1,12 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+resource "azurerm_subnet" "aks" {
+  name                 = "${var.prefix}-aks-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
